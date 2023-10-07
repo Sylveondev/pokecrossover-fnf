d2ff19e357538a0bff16852f0a92d18869bb0b70
MemeHovy <124418090+MemeHovy@users.noreply.github.com>
2023-10-07 03:59:18 -0400

    we're back (compile fixes)

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -55,7 +55,11 @@ import flash.media.Sound;
 
 using StringTools;
 
+#if (flixel < "5.3.0")
 @:access(flixel.system.FlxSound._sound)
+#else
+@:access(flixel.sound.FlxSound._sound)
+#end
 @:access(openfl.media.Sound.__buffer)
 
 class ChartingState extends MusicBeatState
